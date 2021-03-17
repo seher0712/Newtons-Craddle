daaca7f44b2e5165e2f7eba0a33acc0666d3f38f
seher0712 <75379142+seher0712@users.noreply.github.com>
2021-03-17 19:51:35 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,71 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+
+function setup() {
+	createCanvas(1600, 700);
+	
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+
+	bob1 = new bob (720,675,40)
+	bob2 = new bob (760,675,40)
+	bob3 = new bob (800,675,40)
+	bob4 = new bob (840,675,40)
+	bob5 = new bob (880,675,40)
+
+	roof1 = new roof(800,175,230,20)
+
+	rope1 = new rope(bob1.body,roof1.body, -80,0)
+	rope2 = new rope(bob2.body,roof1.body,-40,0)
+	rope3 = new rope(bob3.body,roof1.body,0,0)
+	rope4 = new rope(bob4.body,roof1.body,40,0)
+	rope5 = new rope(bob5.body,roof1.body,80,0)
+
+
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(255);
+  
+  bob1.display();
+  bob2.display();
+  bob3.display();
+  bob4.display();
+  bob5.display();
+
+  roof1.display();
+
+  rope1.display();
+  rope2.display();
+  rope3.display();
+  rope4.display();
+  rope5.display();
+
+  drawSprites();
+ 
+}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:-50,y:-45})
+
+		
+	}
+}
+
+
+
